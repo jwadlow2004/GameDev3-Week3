21df50531aa87037da936f159a31efc50529515c
jwadlow2004 <jgvwadlow@gmail.com>
2024-11-14 12:03:53 +0000

    PlayerDetectedByTower

--- Game Dev 3 - Week 3/Assets/_Scripts/Enemies/Tower/Enemies_Tower_PlayerSpotted.cs
+++ Game Dev 3 - Week 3/Assets/_Scripts/Enemies/Tower/Enemies_Tower_PlayerSpotted.cs
@@ -1,3 +1,4 @@
+using GameDevWithMarco.ObserverPattern;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,12 +8,18 @@ namespace GameDevWithMarco.Enemies
 { 
  public class Enemies_Tower_PlayerSpotted : MonoBehaviour
  {
+
+  [SerializeField] GameEvent playerSpotted;
+
+
+ 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         //If the player enters the collider, the spotted event is raised!
         if (collision.gameObject.tag == "Player")
         {
-            //Do something
+                //Raises the event if "playerSpotted"
+                playerSpotted?.Raise();
         }
     }
  }
